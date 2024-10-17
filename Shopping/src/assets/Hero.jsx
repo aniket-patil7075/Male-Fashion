a77b5b797fe0cf6654d43378619c1b28383e623d
aniket-patil7075 <akp7075@gmail.com>
2024-10-17 11:19:32 +0530

    Changes in Hero text

--- Shopping/src/assets/Hero.jsx
+++ Shopping/src/assets/Hero.jsx
@@ -1,21 +1,48 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 function Hero() {
   return (
     <div className="mainHero">
-      <Carousel fade interval={3000} className="custom-carousel" indicators={false}>
+      <Carousel
+        fade
+        interval={3000}
+        className="custom-carousel"
+        indicators={false}
+      >
         <Carousel.Item>
           <img src="/hero-1.jpg" alt="Hero1" className="caraImg w-100" />
-          <Carousel.Caption>
-            
-          </Carousel.Caption>
+          
+          <div className="caraText w-25">
+            <p className="text-danger fw-bold">SUMMER COLLECTION</p>
+            <h1 className="text-dark fw-bold mt-4">Fall - Winter </h1>
+            <h1 className="text-dark fw-bold">Collections 2024</h1>
+            <p className="text-dark fw-bold mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt aliqua.
+            </p>
+            <Button variant="dark" className="heroButton mt-4  px-4 py-3">
+              SHOP NOW <FaArrowRightLong className="heroArrow ms-1"/>
+            </Button>
+          </div>
         </Carousel.Item>
         <Carousel.Item>
           <img src="/hero-2.jpg" alt="Hero2" className="caraImg w-100" />
-          <Carousel.Caption>
-            
-          </Carousel.Caption>
+
+          <div className="caraText w-25">
+            <p className="text-danger fw-bold">SUMMER COLLECTION</p>
+            <h1 className="text-dark fw-bold mt-4">Fall - Winter </h1>
+            <h1 className="text-dark fw-bold">Collections 2024</h1>
+            <p className="text-dark fw-bold mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt aliqua.
+            </p>
+            <Button variant="dark" className="heroButton mt-4  px-4 py-3">
+              SHOP NOW <FaArrowRightLong className="heroArrow ms-1"/>
+            </Button>
+          </div>
         </Carousel.Item>
       </Carousel>
     </div>
